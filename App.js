f2c82ed636a563ad38d133c7e444096085771c49
Bruno Henrique <brunohenriquex00@gmail.com>
2021-04-12 15:15:48 -0300

    passando regioes para o mapa

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import MapView,{Marker} from 'react-native-maps';
 export default function App() {
+  const [region,setRegion] = useState({
+    latitude: -6.8881626,
+    longitude: -38.5550128,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  function moverCidade(lat,long){
+    setRegion({
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    })
+  }
+
   return (
+    //-6.8881626
+    //-38.5550128
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={{ flexDirection: 'row', marginTop:160 }} >
+          <Button title="Brasilia" onPress={()=>{moverCidade(-15.8080374,-47.8750231)}} />
+          <Button title="São Paulo" onPress={()=>{moverCidade(-23.5492243,-46.5813785)}} />
+        </View>
+
+        <Text>{region.latitude} | {region.longitude}</Text>
+
+        <MapView
+          style={styles.mapView}
+          region={region}
+        >
+          <Marker
+            coordinate={{
+              latitude: region.latitude,
+              longitude: region.longitude,
+            }}
+            title="Entrega"
+            description="Local de entrega"
+            icon={{
+              uri: "https://img.icons8.com/plasticine/1x/truck.png"
+            }}
+          />
+        </MapView>
     </View>
   );
 }
@@ -18,4 +56,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapView:{
+    width: Dimensions.get('window').width,
+    height: '100%'
+  }
 });
